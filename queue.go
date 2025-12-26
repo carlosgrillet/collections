86f229de80051d553e90d86d910718a180a0db03
Carlos Grillet <carlosbeta5000@gmail.com>
2025-12-26 06:42:02 +0100

    add: creating queues and binary trees logic

--- queue.go
+++ queue.go
@@ -1,1 +1,54 @@
 package collections
+
+type Queue[T any] struct {
+	elements []T
+	len int
+}
+
+// Generare a new empty queue
+func NewQueue[T any]() *Queue[T] {
+	return &Queue[T]{}
+}
+
+// Enqueue a new element into the queue
+func (q *Queue[T]) Enqueue(element T) {
+	q.elements = append(q.elements, element)
+	q.len += 1
+}
+
+// Next removes and returns the first element from the queue
+func (q *Queue[T]) Next() (T, bool) {
+	var zero T
+	if q.len == 0 {
+		return zero, false
+	}
+	next := q.elements[0]
+	q.elements = q.elements[1:]
+	q.len -= 1
+	return next, true
+}
+
+// Peek return what is the next element in the queue without removing it
+func (q *Queue[T]) Peek() (T, bool) {
+	var zero T
+	if q.len == 0 {
+		return zero, false
+	}
+	return q.elements[0], true
+}
+
+// Clear remove all elements from the queue
+func (q *Queue[T]) Clear() {
+	q.elements = []T{}
+	q.len = 0
+}
+
+// Len return the current lenght of the queue
+func (q *Queue[T]) Len() int {
+	return q.len
+}
+
+// IsEmpty return true if the queue has no elements
+func (q *Queue[T]) IsEmpty() bool {
+	return len(q.elements) == 0
+}
--- tree.go
+++ tree.go
@@ -1,1 +1,57 @@
 package collections
+
+import "fmt"
+
+type Node[T any] struct {
+	Value T
+	Left  *Node[T]
+	Right *Node[T]
+}
+
+type Tree[T any] struct {
+	root *Node[T]
+}
+
+// Generate a new empty binary tree
+func NewTree[T any]() *Tree[T] {
+	return &Tree[T]{}
+}
+
+// Print all the nodes of a binary tree using BFS algorithm.
+func (t *Tree[T]) String() string {
+	output := ""
+	queue := NewQueue[*Node[T]]()
+	queue.Enqueue(t.root)
+
+	for !queue.IsEmpty() {
+		current_level := []T{}
+		level_size := queue.Len()
+
+		for range level_size {
+			current_node, _ := queue.Next()
+			current_level = append(current_level, current_node.Value)
+			if current_node.Left != nil {
+				queue.Enqueue(current_node.Left)
+			}
+			if current_node.Right != nil {
+				queue.Enqueue(current_node.Right)
+			}
+		}
+		output += fmt.Sprintf("%v\n", current_level)
+	}
+	return output
+}
+
+// MaxDepth return the maximum depth of the tree using DFS algorithm
+func (t *Tree[T]) MaxDepth() int {
+	return DFS(t.root)	
+}
+
+func DFS[T any](root *Node[T]) int {
+	if root == nil {
+		return 0
+	}
+	left := DFS(root.Left)
+	right := DFS(root.Right)
+	return 1 + max(left, right)
+}
